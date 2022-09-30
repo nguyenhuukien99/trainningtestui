f2864771c81aa0836673850ea9336e7c113c9501
Huu_Kien <kien.nguyen01@alobridge.com>
2022-09-30 14:51:53 +0700

    jest test ui

--- src/App.tsx
+++ src/App.tsx
@@ -6,8 +6,8 @@ function App() {
     <div className="App">
       <User
         name="Nguyen Huu Kien"
-        age={18}
-        birthday="20/04/1999"
+        age={23}
+        birthday="22/04/1999"
         src="https://znews-photo.zingcdn.me/w660/Uploaded/unvjuas/2021_01_12/106228561_288966105802103_4617392174817760819_n.jpg"
       />
     </div>
--- src/component/user.test.tsx
+++ src/component/user.test.tsx
@@ -4,24 +4,13 @@ import User from "./user";
 it("renders correctly", () => {
   const tree = renderer
     .create(
-      <User name="Nguyen Huu Kien" age={18} birthday="20/04/1999" src="nancy" />
+      <User
+        name="Nguyen Huu Kien"
+        age={23}
+        birthday="20/04/1999"
+        src="https://znews-photo.zingcdn.me/w660/Uploaded/unvjuas/2021_01_12/106228561_288966105802103_4617392174817760819_n.jpg"
+      />
     )
     .toJSON();
-  expect(tree).toMatchSnapshot(`
-  <UserStyled>
-  <div className="info">
-    <div className="text">
-      <div className="name">Ho va ten:Nguyen Huu Kien</div>
-      <div className="age">Tuoi:18</div>
-      <div className="birthday">Ngay Sinh:20/04/1999</div>
-    </div>
-    <div className="image">
-      <img
-        src="nancy"
-        alt="nancy"
-      />
-    </div>
-  </div>
-</UserStyled>
-  `);
+  expect(tree).toMatchSnapshot();
 });
